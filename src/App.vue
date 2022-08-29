7d367967825a6dcc00f136ac5626192cd5caa186
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-08-29 12:16:10 +0300

    added main nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1 class="text-4xl text-center">Test</h1>
+  <main-nav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
