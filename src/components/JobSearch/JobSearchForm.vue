1687694b86932a7e58e6ec74d0b886dbcb2229f4
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-10-27 23:05:17 +0300

    add two way binding to the input component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,24 +6,23 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
+        <text-input
           placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
+          :value="role"
+          @handle-input="role = $event"
         />
       </div>
+
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
         >in</span
       >
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
+        <text-input
           placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
+          :value="location"
+          @handle-input="location = $event"
         />
       </div>
     </div>
@@ -33,10 +32,12 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 
 export default {
   name: "JobSearchForm",
   components: {
+    TextInput,
     ActionButton,
   },
   data() {
