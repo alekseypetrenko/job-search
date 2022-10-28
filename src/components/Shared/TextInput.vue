5c6cc5ab99df7e27fbbd8d6f1b0bf715d4b5e847
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-10-28 22:58:51 +0300

    update to prop to modelValue

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -2,7 +2,7 @@
   <input
     type="text"
     :placeholder="placeholder"
-    :value="value"
+    :value="modelValue"
     class="w-full text-lg font-normal focus:outline-none"
     @input="handleInput"
   />
@@ -17,15 +17,15 @@ export default {
       required: false,
       default: "",
     },
-    value: {
+    modelValue: {
       type: String,
       required: true,
     },
   },
-  emits: ["handleInput"],
+  emits: ["update:modelValue"],
   methods: {
     handleInput($event) {
-      this.$emit("handleInput", $event.target.value);
+      this.$emit("update:modelValue", $event.target.value);
     },
   },
 };
