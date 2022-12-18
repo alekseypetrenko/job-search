c336ee58d525784a3d923aa6ca40913f179597b6
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-12-18 21:08:29 +0200

    added simple accordion and fas icons

---
+++ src/components/Shared/Accordion.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-5 border-b border-solid border-brand-gray-2">
+    <div
+      class="flex flex-wrap items-center justify-between cursor-pointer"
+      @click="open()"
+    >
+      <h3 class="text-bold font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="createIcon" />
+      <div v-if="isOpen" class="w-full mt-5">Child</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+  computed: {
+    createIcon() {
+      return this.isOpen ? "angle-up" : "angle-down";
+    },
+  },
+};
+</script>
