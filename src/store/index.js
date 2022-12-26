323523116d9fc3c1e17ce91319ab64241782db24
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-12-26 22:15:41 +0200

    move api getJobs from component, create test for it. Create test for mutations and actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore } from "vuex";
+import getJobs from "@/api/getJobs";
 
 export const LOGIN_USER = "LOGIN_USER";
+export const RECIEVE_JOBS = "RECIEVE_JOBS";
+export const FETCH_JOBS = "FETCH_JOBS";
 
 export const state = () => {
   return {
     isLoggedIn: false,
+    jobs: [],
   };
 };
 
@@ -12,13 +16,23 @@ export const mutations = {
   [LOGIN_USER](state) {
     state.isLoggedIn = true;
   },
+  [RECIEVE_JOBS](state, jobs) {
+    state.jobs = jobs;
+  },
+};
+
+export const actions = {
+  [FETCH_JOBS]: async (context) => {
+    const jobListings = await getJobs();
+    context.commit(RECIEVE_JOBS, jobListings);
+  },
 };
 
 const store = createStore({
   state,
   mutations,
+  actions,
   getters: {},
-  actions: {},
   modules: {},
   strict: process.env.NODE_ENV !== "production",
 });
