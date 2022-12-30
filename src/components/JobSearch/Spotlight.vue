94c110a992a2dd05dc17c60e38bc0dedc28d5462
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-12-30 19:14:08 +0200

    added spotlight and tests for it

--- src/components/JobSearch/Spotlight.vue
+++ src/components/JobSearch/Spotlight.vue
@@ -1,11 +1,18 @@
 <template>
   <ul>
-    <li>1</li>
-    <li>2</li>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot
+        :img="spotlight.img"
+        :title="spotlight.title"
+        :description="spotlight.description"
+      ></slot>
+    </li>
   </ul>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Spotlight",
   data() {
@@ -13,9 +20,11 @@ export default {
       spotlights: [],
     };
   },
-  mounted() {
-    // const baseUrl = "localhost:";
-    console.log(1);
+  async mounted() {
+    const baseUrl = process.env.VUE_APP_API_URL;
+    const url = `${baseUrl}/spotlights`;
+    const response = await axios.get(url);
+    this.spotlights = response?.data;
   },
 };
 </script>
---
+++ tests/unit/components/JobSearch/Spotlight.test.js
@@ -0,0 +1,67 @@
+import { mount, flushPromises } from "@vue/test-utils";
+import axios from "axios";
+jest.mock("axios");
+
+import Spotlight from "@/components/JobSearch/Spotlight.vue";
+
+describe("Spotlight", () => {
+  const mockSpotlightResponse = (data = {}) => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          img: "Some image",
+          title: "Title test",
+          description: "description",
+          ...data,
+        },
+      ],
+    });
+  };
+
+  it("provides img to parent component", async () => {
+    const data = { img: "Some Image test" };
+    mockSpotlightResponse(data);
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `
+        <template #default="slotProps">
+        <h1>{{slotProps.img}}</h1>
+        </template>
+        `,
+      },
+    });
+    await flushPromises();
+    console.log(wrapper.text());
+    expect(wrapper.text()).toMatch("Some Image test");
+  });
+  it("provides title to parent component", async () => {
+    const data = { title: "Title test" };
+    mockSpotlightResponse(data);
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `
+        <template #default="slotProps">
+        <h1>{{slotProps.title}}</h1>
+        </template>
+        `,
+      },
+    });
+    await flushPromises();
+    expect(wrapper.text()).toMatch("Title test");
+  });
+  it("provides description to parent component", async () => {
+    const data = { description: "Some description" };
+    mockSpotlightResponse(data);
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `
+        <template #default="slotProps">
+        <h1>{{slotProps.description}}</h1>
+        </template>
+        `,
+      },
+    });
+    await flushPromises();
+    expect(wrapper.text()).toMatch("Some description");
+  });
+});
