6258a4043b391cf246c746965171fdec642f22c4
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-02-10 21:21:52 +0200

    added actions and test

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,11 @@
-import { FETCH_JOBS, RECIEVE_JOBS } from "@/store/constants";
+import {
+  FETCH_JOBS,
+  RECIEVE_JOBS,
+  FETCH_DEGREES,
+  RECIEVE_DEGREES,
+} from "@/store/constants";
 import getJobs from "@/api/getJobs";
-// import getDegrees from "@/api/getDegrees";
+import getDegrees from "@/api/getDegrees";
 
 import { Commit } from "vuex";
 
@@ -13,10 +18,10 @@ const actions = {
     const jobListings = await getJobs();
     context.commit(RECIEVE_JOBS, jobListings);
   },
-  // [FETCH_JOBS]: async (context: Context) => {
-  //   const jobListings = await getJobs();
-  //   context.commit(RECIEVE_JOBS, jobListings);
-  // },
+  [FETCH_DEGREES]: async (context: Context) => {
+    const degrees = await getDegrees();
+    context.commit(RECIEVE_DEGREES, degrees);
+  },
 };
 
 export default actions;
--- tests/unit/store/actions.test.ts
+++ tests/unit/store/actions.test.ts
@@ -1,9 +1,12 @@
 import actions from "@/store/actions";
 import getJobs from "@/api/getJobs";
+import getDegrees from "@/api/getDegrees";
 
 jest.mock("@/api/getJobs");
+jest.mock("@/api/getDegrees");
 
 const getJobsMock = getJobs as jest.Mock;
+const getDegreesMock = getDegrees as jest.Mock;
 
 describe("actions", () => {
   describe("FETCH_JOBS", () => {
@@ -31,6 +34,35 @@ describe("actions", () => {
         {
           id: 1,
           title: "Developer",
+        },
+      ]);
+    });
+  });
+  describe("FETCH_DEGREES", () => {
+    beforeEach(() => {
+      getDegreesMock.mockResolvedValue([
+        {
+          id: 1,
+          degree: "Bachelor",
+        },
+      ]);
+    });
+
+    it("makes request to fetch degrees", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_DEGREES(context);
+      expect(getJobs).toHaveBeenCalled();
+    });
+
+    it("sends message to save recieved degrees in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_DEGREES(context);
+
+      expect(commit).toHaveBeenCalledWith("RECIEVE_DEGREES", [
+        {
+          id: 1,
+          degree: "Bachelor",
         },
       ]);
     });
