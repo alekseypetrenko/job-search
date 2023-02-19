9c3ed4050f4fc2bb2ad21544ecbe99713cf332f3
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-02-19 21:05:47 +0200

    added font-awesome package and register in the main

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
 // import { createApp } from "vue";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import {
-//   faSearch,
-//   faAngleDown,
-//   faAngleUp,
-// } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSearch,
+  faAngleDown,
+  faAngleUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/assets/tailwind.css";
 // import App from "./App.vue";
 // import router from "@/router";
 // import store, { key } from "@/store";
 
-// library.add(faSearch);
-// library.add(faAngleDown);
-// library.add(faAngleUp);
+library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 
 // createApp(App)
 //   .use(store, key)
 //   .use(router)
-//   .component("FontAwesomeIcon", FontAwesomeIcon)
+
 //   .mount("#app");
 
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).mount("#app");
